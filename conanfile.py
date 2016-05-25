941ced83dd5bf26183427515547076355934457e
Lasote <lasote@gmail.com>
2016-05-25 20:19:35 +0200

    Mt try 2

--- conanfile.py
+++ conanfile.py
@@ -62,10 +62,11 @@ class JanssonConan(ConanFile):
             cmake = CMake(self.settings)
             shared_options = "-DJANSSON_BUILD_SHARED_LIBS=ON" if self.options.shared else "-DJANSSON_BUILD_SHARED_LIBS=OFF"
             other_options = "-DJANSSON_BUILD_DOCS=OFF"
-            if str(self.settings.runtime) in ("MT", "MTd"):
-                other_options += " -DSTATIC_CRT=ON -DJANSSON_STATIC_CRT=ON"
-            else:
-                other_options += " -DSTATIC_CRT=OFF -DJANSSON_STATIC_CRT=OFF"
+            if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+                if str(self.settings.compiler.runtime) in ("MT", "MTd"):
+                    other_options += " -DSTATIC_CRT=ON -DJANSSON_STATIC_CRT=ON"
+                else:
+                    other_options += " -DSTATIC_CRT=OFF -DJANSSON_STATIC_CRT=OFF"
                  
             self.run("cd %s && mkdir _build" % self.ZIP_FOLDER_NAME)
             cd_build = "cd %s/_build" % self.ZIP_FOLDER_NAME
